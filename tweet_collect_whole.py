319ee26e7abb86b613a92c036c0a2ced857c372a
annebaril <45000845+annebaril@users.noreply.github.com>
2018-11-15 11:49:08 +0100

    Add files via upload

---
+++ tweet_collect_whole.py
@@ -0,0 +1,26 @@
+def get_candidate_queries(num_candidate, file_path):
+    """
+    Generate and return a list of string queries for the search Twitter API from the file file_path_num_candidate.txt
+    :param num_candidate: the number of the candidate
+    :param file_path: the path to the keyword and hashtag
+    files
+    :param type: type of the keyword, either "keywords" or "hashtags"
+    :return: (list) a list of string queries that can be done to the search API independently
+    """
+    hashtag = file_path + "/hashtag_candidate_" + str(num_candidate) +".txt"
+
+    try:
+        print(hashtag)
+        hashtag_candidat = open(hashtag,'r')
+        liste_hashtags = []
+        for t in hashtag_candidat :
+            liste_hashtags.append(t)
+        return liste_hashtags
+
+    except IOError:
+        print('Cannot open')
+
+#print(get_candidate_queries(1976143068,"Users/anne/Desktop/CandidateData"))
+
+
+
