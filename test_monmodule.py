8b517584d52ec131d2ca234aa31ab13505ebdacd
gautier12 <45004509+gautier12@users.noreply.github.com>
2018-11-16 16:54:39 +0100

    Update test_monmodule.py

--- test_monmodule.py
+++ test_monmodule.py
@@ -1,10 +1,15 @@
 from twitter_collect.twitter_connection_setup import *
-
 def test_fct():
     assert None!= twitter_setup()
 
+from twitter_analysis.fonctionnalite9 import *
 
+assert opinion_surcandidat(['Negative','Neutral','positive'] === (['positive','Neutral','Negative'])
+                           
+from twitter_analysis.fonctionnalite8 import *
 
-
-
-
+assert liste_en_texte(['salut','ça','va']) == ' salut ça va'
+                           
+                           
+                          
+                        
